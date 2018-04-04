512574300362272225aae4fd154239c5ce43f2c8
John Hennessy <usmcduck101@gmail.com>
2018-04-04 14:01:13 -0700

    Adding lesson 25 and updating 24 with logging. 24 now logs each card when dealt to a text file.

--- Tech Academy CS Projects/CS_24_TwentyOneGame/CS_24_TwentyOneGame/Dealer.cs
+++ Tech Academy CS Projects/CS_24_TwentyOneGame/CS_24_TwentyOneGame/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CS_24_TwentyOneGame
 {
@@ -16,7 +17,15 @@ namespace CS_24_TwentyOneGame
         {
             //Adds the first/top card to the hand:
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            //Logging everytime a card is dealt:
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            //Logging:
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Logs\log.txt", true))
+            {
+                file.WriteLine(card); 
+            } //Once the code hits here the Garbage Collector gets rid of the above code.
+            //That's what the using statement does. 
+                Console.WriteLine(card);
             //Removes the added card from the deck:
             //This removes that card at index 0, which is the first/top card.
             Deck.Cards.RemoveAt(0);
---
+++ Tech Academy CS Projects/CS_25_File_IO/CS_25_File_IO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace CS_25_File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Here is some text.";
+            //Makes a text file called log in the folder Log.
+            //File.WriteAllText(@"C:\Users\Student\Logs\log.txt");
+            //If you hover over 'text' you see "Here is some text."
+
+            File.ReadAllText(@"C:\Users\Student\Logs\log.txt");
+            Console.ReadLine();
+
+        }
+    }
+}
