d68cd88c428de9e9c22f4bab4a987b1cfc651a8a
John Hennessy <usmcduck101@gmail.com>
2018-04-09 17:37:28 -0700

    Adding MVC lesson 1-6 (all done in one file). Intro to Razor and MVC.

---
+++ DevU MVC/HelloMVC/HelloMVC/Models/HelloModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class HelloModel
+    {
+        public static List<Car> GetCars()
+        {
+            List<Car> cars = new List<Car>
+            {
+                new Car { Id = 1, Make = "BMW", Model = "528i" }, //Object initializer.
+                new Car { Id = 2, Make = "Hyundai", Model = "Eleantra" },
+                new Car { Id = 3, Make = "Toyota", Model = "4Runner" }
+            }; //Collection initializer.
+            return cars;
+        }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+    }
+}
