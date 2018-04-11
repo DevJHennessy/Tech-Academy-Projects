97a5fcd30e27c1899fd11898ac916a4436dde186
John Hennessy <usmcduck101@gmail.com>
2018-04-11 16:11:20 -0700

    Adding MVC lesson 16 on Html Helper Methods.

---
+++ DevU MVC/ACME/ACME/Models/ProductMetadata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ACME.Models
+{
+    [MetadataType(typeof(Product.Metadata))]
+    public partial class Product
+    {
+        //Nobody can inherit of extend this class, so it is sealed:
+        sealed class Metadata
+        {
+            [Key]
+            public System.Guid ProductId { get; set; }
+
+            [Required]
+            [Display(Name="Product Name")]
+            [StringLength(10)]
+            public string Name { get; set; }
+
+            [Required]
+            [RegularExpression(@"^\$?\d+(\.(\d{2}))?$")]
+            public decimal Price { get; set; }
+        }
+    }
+}
